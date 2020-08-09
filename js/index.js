8a7353da6a5fbe748deae18f59c833a2cf5175f0
Francisco <franciscoveloz245@gmail.com>
2020-08-09 12:08:02 -0500

    Hicimos responsive la nav

---
+++ js/index.js
@@ -0,0 +1,7 @@
+document.querySelector(".menu-btn").addEventListener("click", () => {
+    document.querySelector(".nav__menu").classList.toggle("show")
+})
+
+// document.querySelector(".menu-btn").addEventListener("click", () => {
+//     document.querySelector(".nav-main__menu").classList.toggle("show");
+//   });
