9e8842d37b01531f52f48debee3b6135e39925f7
Francisco Veloz <franciscoveloz245@gmail.com>
2020-09-20 19:55:29 -0500

    Modificamos duracion de animacines

--- js/index.js
+++ js/index.js
@@ -7,59 +7,59 @@ document.querySelector(".menu-btn").addEventListener("click", () => {
 window.sr = ScrollReveal();
 
 sr.reveal(".nav-main", {
-  duration: 2000,
+  duration: 1000,
   origin: "bottom",
 });
 
 sr.reveal(".menu-btn", {
-  duration: 2000,
+  duration: 1000,
   origin: "bottom",
 });
 
 
 sr.reveal(".header__img", {
-  duration: 2000,
+  duration: 1000,
   origin: "left",
   distance: "300px",
 });
 
 sr.reveal(".header .content", {
-  duration: 2000,
+  duration: 1000,
   origin: "right",
   distance: "300px",
 });
 
 sr.reveal(".testimonial", {
-	duration: 2000,
+	duration: 1000,
 	origin: "left",
 	viewFactor: 0.2
 });
 
 sr.reveal(".info-one__img", {
-  duration: 2000,
+  duration: 1000,
   origin: "left",
 	distance: "300px",
-	delay: 400
+	delay: 200
 });
 
 sr.reveal(".info-one .content", {
-  duration: 2000,
+  duration: 1000,
   origin: "right",
 	distance: "300px",
-	delay: 400
+	delay: 200
 });
 
 sr.reveal(".info-two", { delay: 400 });
 
 sr.reveal(".contact__img", {
-  duration: 2000,
+  duration: 1000,
   origin: "right",
 	distance: "300px",
 	delay: 400
 });
 
 sr.reveal(".contact .content", {
-  duration: 2000,
+  duration: 1000,
   origin: "left",
 	distance: "300px",
 	delay: 400
